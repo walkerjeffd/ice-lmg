0255db6738af416c0c0de706bbd61e1335b1a8db
Jeff Walker <walker.jeff.d@gmail.com>
2019-11-13 10:39:15 -0500

    update gage-primary dataset

--- r/theme-gage-primary.R
+++ r/theme-gage-primary.R
@@ -16,8 +16,7 @@ theme$config$citations <- unname(map(themes, ~ .$theme$config$citations[[1]]))
 # merge variables ---------------------------------------------------------
 
 df_variables <- map_df(themes, ~ .$variables$df) %>% 
-  filter(primary) %>% 
-  filter(theme != "gage-qtrend")
+  filter(primary)
 
 cfg_variables <- transform_variables(df_variables)
 
@@ -40,22 +39,21 @@ variables_gage_qstat <- themes$`gage-qstat`$variables$df %>%
 df_gage_qstat <- themes$`gage-qstat`$dataset$out %>% 
   select(id, decade, variables_gage_qstat)
 
-# variables_gage_qtrend <- themes$`gage-qtrend`$variables$df %>% 
-#   filter(primary) %>% 
-#   pull(id)
-# df_gage_qtrend <- themes$`gage-qtrend`$dataset$out %>% 
-#   select(id, decade, mklevel, variables_gage_qtrend) %>% 
-#   filter(
-#     mklevel %in% c("Fall", "Winter", "Spring", "Summer", "Q10", "Q50", "Q90")
-#   ) %>% 
-#   mutate(
-#     mklevel = str_c("mk_slope_", tolower(mklevel))
-#   ) %>% 
-#   pivot_wider(names_from = "mklevel", values_from = "mk_slope")
+variables_gage_qtrend <- themes$`gage-qtrend`$variables$df %>%
+  filter(primary) %>%
+  pull(id)
+df_gage_qtrend <- themes$`gage-qtrend`$dataset$out %>%
+  select(id, decade, variables_gage_qtrend)
 
 df <- df_gage_cov %>% 
-  full_join(df_gage_qstat, by = c("id", "decade"))
-  # full_join(df_gage_qtrend, by = c("id", "decade"))
+  full_join(df_gage_qstat, by = c("id", "decade")) %>% 
+  full_join(df_gage_qtrend, by = c("id", "decade")) %>% 
+  left_join(
+    themes$`gage-cov`$layer$df %>% 
+      select(id, lat = dec_lat_va, lon = dec_long_va),
+    by = "id"
+  ) %>% 
+  select(id, lat, lon, decade, everything())
 
 dataset <- list(
   out = df
@@ -75,6 +73,7 @@ export_theme(theme, variables, dataset, layer)
 # feature data ------------------------------------------------------------
 
 df_feature <- dataset$out %>% 
+  select(-lat, -lon) %>% 
   nest(values = -id) %>% 
   mutate(
     values = map(values, function (x) {
