058652b97f0f0238e1d2d0534b3aa4ab01e9472f
Jeff Walker <walker.jeff.d@gmail.com>
2020-01-15 10:12:41 -0500

    welch revisions

--- src/components/Disclaimer.vue
+++ src/components/Disclaimer.vue
@@ -1,6 +1,11 @@
 <template>
   <v-alert type="warning" outlined prominent icon="mdi-alert" class="mb-0">
     <h3>Disclaimer</h3>
-    <span class="body-2">This information is preliminary and is subject to revision. It is being provided to meet the need for timely best science. The information is provided on the condition that neither the U.S. Geological Survey nor the U.S. Government may be held liable for any damages resulting from the authorized or unauthorized use of the information.</span>
+    <span class="body-2">
+      This information is preliminary and is subject to revision. It is being provided to meet the need for
+      timely best science. The information is provided on the condition that neither the U.S. Geological Survey nor the U.S.
+      Government may be held liable for any damages resulting from the authorized or unauthorized use of the information.
+      Any use of trade, firm, or product names is for descriptive purposes only and does not imply endorsement by the U.S. Government.
+    </span>
   </v-alert>
 </template>
