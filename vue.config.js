31b5286421f6fd92d5909b943cb13cdca00c59f0
Walker <jdwalker@usgs.gov>
2020-02-24 10:22:49 -0500

    use BASE_URL env for publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/lmg-restore/' : '/',
+  publicPath: process.env.BASE_URL || '/',
   'transpileDependencies': [
     'vuetify'
   ]
